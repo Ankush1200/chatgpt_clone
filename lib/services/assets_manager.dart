dddfac7dc24202c77cfba390ff7e39126a259185
Ankush Prajapati <ankushpra49054@gmail.com>
2024-02-07 03:58:11 +0530

    first-commit

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,6 @@
+class AssetsManagesr{
+  static String imagepath="assets/images";
+  static String chatlogo="$imagepath/chat_logo.png";
+  static String openAIlogo="$imagepath/openai_logo.png";
+  static String personlogo="$imagepath/person.png";
+}
