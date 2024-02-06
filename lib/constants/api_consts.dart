f147deafc9d3281d7d8175e5fce7a2580ab642af
Ankush Prajapati <ankushpra49054@gmail.com>
2024-02-07 04:27:36 +0530

    Actual color theam added

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "cf-ccVmb8csTsnfsVxyRrPRaT3BlbkFJFo8tmGMtZ0asfbsjffg";
